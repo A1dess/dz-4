a12273c1bb5218db98a5b347263f6286a90aba3a
A1dess <aidosberdibaev.ru@gmail.com>
2023-04-15 18:01:28 +0600

    'ewrwrw'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutUs from'./AboutUs/AboutUs'
+function App() {
+  return (
+    <div className="App">
+    <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
